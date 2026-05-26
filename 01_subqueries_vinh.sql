7b1cb5685f2b44a8f03dfdebd2e930e1ee1436bd
Phung Quoc Vinh <quocvinh.cde@gmail.com>
2026-05-26 15:20:08 +0700

    Advanced SQL Practice - Database Setup (MySQL)    Database: advanced_sql_practice    Cre: https://github.com/santoshkhatri9860/advanced-sql-practice-mysql
       new up 2605

--- 01_subqueries_vinh.sql
+++ 01_subqueries_vinh.sql
@@ -86,50 +86,89 @@ WHERE salary >= (SELECT MAX(Employees.salary) FROM Employees JOIN Departments ON
 whose salary is greater than or equal to
 the average salary in Finance department.
 */
+SELECT Employees.emp_name
+FROM Employees
+WHERE salary >= (SELECT AVG(Employees.salary) FROM Employees JOIN Departments ON Employees.dept_id = Departments.dept_id WHERE dept_name = 'Finance')
 
 /* 
 Display employee names
 whose salary is less than
 the highest salary in the Marketing department.
 */
-
+SELECT Employees.emp_name
+FROM Employees
+WHERE salary < (SELECT MAX(Employees.salary) FROM Employees JOIN Departments ON Employees.dept_id = Departments.dept_id WHERE dept_name = 'Marketing')
 /* Display employee names
 whose salary is equal to
 the minimum salary in the HR department.
 */
-
+SELECT Employees.emp_name
+FROM Employees
+WHERE salary = (SELECT MIN(Employees.salary) FROM Employees JOIN Departments ON Employees.dept_id = Departments.dept_id WHERE dept_name = 'HR')
 /*
 Display employee names
 whose salary is NOT equal to
 the highest salary in the IT department.
 */
-
+SELECT Employees.emp_name
+FROM Employees
+WHERE salary != (SELECT MAX(Employees.salary) FROM Employees JOIN Departments ON Employees.dept_id = Departments.dept_id WHERE dept_name = 'IT')
+/*
 /* Display employee names
 whose salary is not equal to
 the minimum salary in the Finance department.
 */
-
+SELECT Employees.emp_name
+FROM Employees
+WHERE salary != (SELECT MIN(Employees.salary) FROM Employees JOIN Departments ON Employees.dept_id = Departments.dept_id WHERE dept_name = 'Finance')
 /* Display employee names
 whose salary is equal to
-the maximum salary in the department where David works.
+the maximum salary in the department where Deepak works.
 */
+
+SELECT Employees.emp_name
+FROM Employees
+WHERE salary = (SELECT MAX(SALARY)
+                FROM Employees
+                WHERE dept_id = (SELECT Departments.dept_id
+                                 FROM Employees
+                                          JOIN Departments ON Employees.dept_id = Departments.dept_id
+                                 WHERE emp_name LIKE 'Deepak'))
 
 /*
 Display department names
-where the average salary is less than Grace’s salary.
+where the average salary is less than Ralph’s salary.
 */
-
+SELECT dept_name
+FROM Departments d
+JOIN Employees e ON d.dept_id = e.dept_id
+GROUP BY 1
+HAVING AVG(salary) < (SELECT salary FROM Employees WHERE emp_name = 'Ralph')
 /*
 Display department names
 where the maximum salary
 is greater than Grace’s salary.
 */
+SELECT dept_name
+FROM Departments d
+JOIN Employees e ON d.dept_id = e.dept_id
+GROUP BY 1
+HAVING AVG(salary) > (SELECT salary FROM Employees WHERE emp_name = 'Hank')
+
 
 /*
 Display department names
 where the minimum salary
 is less than Grace’s salary.
 */
+SELECT d.dept_name, MIN(e.salary) AS MIN
+FROM Departments d
+JOIN Employees e ON e.dept_id = d.dept_id
+GROUP BY 1
+HAVING MIN < (SELECT salary FROM Employees WHERE emp_name = 'Jonas')
+
+
+
 
 /*
 Display department names
@@ -137,8 +176,15 @@ where the average salary
 is equal to the average salary
 of the Sales department.
 */
-
+SELECT Departments.dept_name, AVG(Employees.salary) AS AVG
+FROM Departments
+LEFT JOIN Employees ON Departments.dept_id = Employees.dept_id
+GROUP BY 1
+HAVING AVG = (SELECT AVG(Employees.salary) FROM Departments JOIN Employees ON Departments.dept_id = Employees.dept_id WHERE dept_name = 'Sales')
 /*
 Display employee names
 who belong to a department that exists in the Departments table.
 */
+SELECT Employees.emp_name
+FROM Employees
+WHERE dept_id IN (SELECT dept_id FROM Departments)
